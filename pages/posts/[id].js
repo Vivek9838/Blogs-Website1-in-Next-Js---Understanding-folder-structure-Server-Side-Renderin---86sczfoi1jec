3a01aea9c404a8988809196e80a77dbc5f562151
VIVEK <shsrna41@gmail.com>
2024-03-30 12:43:43 +0530

    vf

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,10 @@ import { useRouter } from 'next/router';
 
 function PostDetail() {
   // Complete the function
+  const router=useRouter();
+  return(
+    <h1 id={router.query.id}>Post Detail: {router.query.id}</h1>
+  )
 }
 
 export default PostDetail;
